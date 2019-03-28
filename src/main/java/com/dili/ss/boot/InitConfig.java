b34c6de21ed8d56363dcc3f293c6f4be9999f899
wangmi <wangmi@diligrp.com>
2019-03-28 09:54:48 +0800

    添加启动配置类

---
+++ src/main/java/com/dili/ss/boot/InitConfig.java
@@ -0,0 +1,13 @@
+package com.dili.ss.boot;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class InitConfig {
+    @PostConstruct
+    public void init(){
+
+    }
+}
